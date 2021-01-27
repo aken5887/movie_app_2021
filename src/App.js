3537c67c6ed555aafd89e3fdbaef9e1331334cc6
aken5887 <aken@kakao.com>
2021-01-27 17:37:00 +0900

    react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count : 0
+  };
+  /**
+   * state는 object이다
+   * state 변경시 render 함수를 재호출하기 위해서 setState를 사용해야함
+   * setState를 호출할때마다 react는 새로운 state와 함께 render function을 호출함
+   */
+  add = () => {
+    console.log('add');
+    // this.setState({count : this.state.count + 1})
+    this.setState(current => ({count : current.count + 1}));
+  };
+
+  minus = () => {
+    console.log('minus');
+    this.setState({count : this.state.count - 1})
+  };
+
+  render(){
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>plus</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
