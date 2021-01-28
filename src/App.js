ae1bc792f2663e1488eb3750a0f924cc8a71f38b
aken5887 <aken@kakao.com>
2021-01-28 14:47:24 +0900

    #6.3 Sharing Props Between Routes

--- src/App.js
+++ src/App.js
@@ -1,73 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
-/**
- * state는 object이다
- * state 변경시 render 함수를 재호출하기 위해서 setState를 사용해야함
- * setState를 호출할때마다 react는 새로운 state와 함께 render function을 호출함
- */
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    console.log(movies);
-    this.setState({ movies: movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    console.log("componentDidMount");
-    // setTimeout(()=>{
-    //     this.setState({isLoading : false});
-    // }, 6000);
-    // fetch
-    // axios
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader __text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-          {
-            movies.map((movie) => {
-              console.log(movie);
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres = {movie.genres}
-                />
-              );
-            })
-          }
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
